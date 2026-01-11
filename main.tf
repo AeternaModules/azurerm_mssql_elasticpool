31340a0006256faa5eae8fa286974963c865fc10
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:45:38 +0000

    Update to schema version v4.57.0

---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "azurerm_mssql_elasticpool" "mssql_elasticpools" {
+  for_each = var.mssql_elasticpools
+
+  location                       = each.value.location
+  name                           = each.value.name
+  resource_group_name            = each.value.resource_group_name
+  server_name                    = each.value.server_name
+  enclave_type                   = each.value.enclave_type
+  license_type                   = each.value.license_type
+  maintenance_configuration_name = each.value.maintenance_configuration_name
+  max_size_bytes                 = each.value.max_size_bytes
+  max_size_gb                    = each.value.max_size_gb
+  tags                           = each.value.tags
+  zone_redundant                 = each.value.zone_redundant
+
+  per_database_settings {
+    max_capacity = each.value.per_database_settings.max_capacity
+    min_capacity = each.value.per_database_settings.min_capacity
+  }
+
+  sku {
+    capacity = each.value.sku.capacity
+    family   = each.value.sku.family
+    name     = each.value.sku.name
+    tier     = each.value.sku.tier
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,51 @@
+variable "mssql_elasticpools" {
+  description = <<EOT
+Map of mssql_elasticpools, attributes below
+Required:
+    - location
+    - name
+    - resource_group_name
+    - server_name
+    - per_database_settings (block):
+        - max_capacity (required)
+        - min_capacity (required)
+    - sku (block):
+        - capacity (required)
+        - family (optional)
+        - name (required)
+        - tier (required)
+Optional:
+    - enclave_type
+    - license_type
+    - maintenance_configuration_name
+    - max_size_bytes
+    - max_size_gb
+    - tags
+    - zone_redundant
+EOT
+
+  type = map(object({
+    location                       = string
+    name                           = string
+    resource_group_name            = string
+    server_name                    = string
+    enclave_type                   = optional(string)
+    license_type                   = optional(string)
+    maintenance_configuration_name = optional(string, "SQL_Default")
+    max_size_bytes                 = optional(number)
+    max_size_gb                    = optional(number)
+    tags                           = optional(map(string))
+    zone_redundant                 = optional(bool)
+    per_database_settings = object({
+      max_capacity = number
+      min_capacity = number
+    })
+    sku = object({
+      capacity = number
+      family   = optional(string)
+      name     = string
+      tier     = string
+    })
+  }))
+}
+
